d5116da66a11df89ca43e743575fcbce065adf48
matthew <matthewchambers763@gmail.com>
2024-10-11 17:19:07 +0200

    getComputerChoice added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello World external")
+console.log("Hello World")
+function getComputerChoice(params) {
+    var params = ['rock','paper','scissors'][Math.floor(Math.random() * 3)];
+}
+console.log(params)
